ccdc0c14b753003f0b25d4abaca266674ff1189c
jo-svg <edwardsjohnny64@gmail.com>
2023-02-18 07:36:54 -0800

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/*The main function is the entry point of the program
+*It returns an integer value to indicate the status of the program
+*In this program, it calls the puts function to print a string to the console
+and then returns 0 to indicate successful completion*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
- }
+/*Intialisation of string*/
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
 
