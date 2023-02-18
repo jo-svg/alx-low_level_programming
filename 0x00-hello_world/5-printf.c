f38d2c0f4a04af4241323610edc06fa7a5a6f963
jo-svg <edwardsjohnny64@gmail.com>
2023-02-18 10:06:52 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Prints using printf() command
+* Return: 0 prints phrase
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,/n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
