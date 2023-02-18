327373b4e2bfc5049aa611ef2c05d3f3e9ae14e3
jo-svg <edwardsjohnny64@gmail.com>
2023-02-18 07:57:59 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
-/*The main function is the entry point of the program
-*It returns an integer value to indicate the status of the program
-*In this program, it calls the puts function to print a string to the console
-and then returns 0 to indicate successful completion*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-/*Intialisation of string*/
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle")
 return (0);
 }
 
