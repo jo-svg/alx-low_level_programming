4334262872db183acd3d528f2a4352426f940b4e
jo-svg <edwardsjohnny64@gmail.com>
2023-02-24 13:02:18 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * _supper -upper letters
+ * 0:char to check
+ *
+ */
+int _isupper(int c){
+	if(c >= 'A' && c <='Z')
+		return(1);
+	else
+		return(0);
+
+
+}
