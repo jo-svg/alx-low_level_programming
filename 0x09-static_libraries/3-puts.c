aee8564b623c2dd5443fb04688ebb493baeeea79
jo-svg <edwardsjohnny64@gmail.com>
2023-03-10 14:40:17 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts- A function that prints a string
+ * @str: The pointer variable
+ *
+ * Return: void
+ * Student: Adewale
+ */
+void _puts(char *str)
+{
+	int strlen = 0;
+
+	while (str[strlen] != '\0')
+	{
+		_putchar(str[strlen]);
+		strlen++;
+	}
+	_putchar('\n');
+}
+
+
+
